279a0c1db2ef32c6a091c62da69d8ddb1578df58
Ashish Ankit <iamankiiit@gmail.com>
2026-06-28 02:57:32 +0530

    Name updates

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'onecognizantui' title`, () => {
+  it(`should have the 'nexusui' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('onecognizantui');
+    expect(app.title).toEqual('nexusui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, onecognizantui');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, nexusui');
   });
 });
